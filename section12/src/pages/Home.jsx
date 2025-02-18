ce3b6ce218a1419ad0a11445efe955f342bf54fb
HAMNIE <ydj.khm@gmail.com>
2025-02-18 16:39:09 +0900

    ~ 12.12) Home 페이지 구현하기

--- section12/src/pages/Home.jsx
+++ section12/src/pages/Home.jsx
@@ -9,7 +9,7 @@ import DiaryList from "../components/DiaryList";
 const getMonthlyData = (pivotDate, data) => {
 
   const beginTime = new Date(pivotDate.getFullYear(), pivotDate.getMonth(), 1, 0, 0, 0).getTime();
-  const endTime = new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1, 23, 59, 59).getTime();
+  const endTime = new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1, 0, 23, 59, 59).getTime();
   
   return data.filter((item) =>
     beginTime <= item.createdDate && item.createdDate <= endTime
